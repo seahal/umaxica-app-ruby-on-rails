e136a170ed0cf16dc8e01d88bd88b3b8900a90bb
seahal <seahal.com@gmail.com>
2024-08-08 20:36:52 +0900

    Learning Rails today. (3)

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Animal < ApplicationRecord
+  attribute :full_name, :string, default: ''
+  attribute :description, :text, default: '???'
+  attribute :extinct,:boolean, default: false
+
   validates :full_name,
             presence: true,
             uniqueness: { case_sensitive: true },
