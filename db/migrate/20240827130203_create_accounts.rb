7c6c998bbf896d575205cf0ce3b74271fb629395
SEAHAL <1926242+seahal@users.noreply.github.com>
2024-08-28 02:30:15 +0900

    I introduced postgres's hash partition
    # sample
    User.insert_all(100000.times.map { {id: SecureRandom.uuid_v7} })
    (0..255).map.with_index(0) { [_2, User.find_by_sql("select * from accounts_p#{ sprintf('%02x', _1) };").count] }

--- db/migrate/20240827130203_create_accounts.rb
+++ db/migrate/20240827130203_create_accounts.rb
@@ -1,13 +1,44 @@
 # frozen_string_literal: true
+PARTITION_SIZE = 255
 
 class CreateAccounts < ActiveRecord::Migration[7.2]
-  def change
+  # def change
+
+  #   create_table :accounts, id: false do |t|
+  #     t.uuid :id, default: SecureRandom.uuid_v7 # FIXME: 本当は DB の関数から uuid を作成したい
+  #     t.string :type
+  #     t.timestamps
+  #   end
+  # end
+
+  def up
     enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
 
-    create_table :accounts, id: false do |t|
-      t.uuid :id, default: SecureRandom.uuid_v7 # FIXME: 本当は DB の関数から uuid を作成したい
-      t.string :type
-      t.timestamps
+    execute <<-SQL
+                CREATE TABLE accounts(
+                    id uuid NOT NULL DEFAULT NULL primary key,
+                    type varchar not null,
+                    created_at timestamp(6) not null,
+                    updated_at timestamp(6) not null
+                ) PARTITION BY HASH (id);
+
+                alter table accounts
+                    owner to "default";
+    SQL
+
+    # FIXME: I'm not quite sure if this size is appropriate.
+    (0..PARTITION_SIZE).each do |i|
+      execute <<-SQL
+        CREATE TABLE accounts_p#{ sprintf('%02x', i) } PARTITION OF accounts FOR VALUES WITH (MODULUS #{PARTITION_SIZE + 1}, REMAINDER #{i});
+      SQL
     end
+  end
+
+  def down
+    execute <<-SQL
+        DROP TABLE accounts;
+    SQL
+
+    disabl_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
   end
 end
