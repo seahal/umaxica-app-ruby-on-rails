aa4343fba9f3d6d7cc1e0840b812b2bd41ec6098
SEAHAL <1926242+seahal@users.noreply.github.com>
2024-08-28 04:42:41 +0900

    now creating Authentication System...

--- config/initializers/simplecov.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-return unless Rails.env.test?
-
-require 'simplecov'
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # root to: redirect('/articles')
   root 'roots#index'
 
+  # For Health Check
   get 'status' => 'rails/health#show', as: :rails_health_check
-
-  resources :connects
-  resources :articles, only: [:index]
-  resources :accounts, only: [:index]
-  resources :sessions, only: [:index]
-  resources :books
-  resources :people
-
-  get 'study/hello_importmap'
-  get 'study/new_turbo'
-  post 'study/create_turbo'
-
-  # samples of learning
-  resources :animals
 end
