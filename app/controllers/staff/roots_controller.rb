f7e97cd36dab310426675ec1f2a6b7e00d26b04b
SEAHAL <1926242+seahal@users.noreply.github.com>
2024-08-29 06:39:03 +0900

    [checkpoint] take a rest...

---
+++ app/controllers/staff/roots_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+
+class Staff::RootsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,39 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "roots#index"
 
-  # For Health Check
-  get "status" => "rails/health#show", as: :rails_health_check
+  # For User
+  constraints ->(req) { req.host == ENV["RAILS_USER_URL"] } do
+    scope module: :user, as: :user do
+      # Health check
+      get "status" => "rails/health#show", as: :rails_health_check
+      get '/'      => "rails/health#show"
+      resource :deactivate, except: :show
+    end
 
-  # following namespaces are for USER's Sign sign_in, up, out. Not for Staff's Session
-  # namespace :sign do
-  #   resources :up # FIXME: scope out this resource
-  #   resources :sign_in # FIXME: scope out this resource
-  #   resource :out, only: [ :show, :edit, :destroy ]
-  # end
-  #
-  resource :sign, only: :index do
-    resources :up, controller: :sign_up # FIXME: scope out this resource
-    resources :in, controller: :sign_in # FIXME: scope out this resource
-    resource :out, only: [ :show, :edit, :destroy ], controller: :sign_out
+    # following namespaces are for USER's Sign sign_in, up, out. Not for Staff's Session
+    # namespace :sign do
+    #   resources :up # FIXME: scope out this resource
+    #   resources :sign_in # FIXME: scope out this resource
+    #   resource :out, only: [ :show, :edit, :destroy ]
+    # end
+    #
+    # resource :sign, only: :index do
+    #   resources :up, controller: :sign_up # FIXME: scope out this resource
+    #   resources :in, controller: :sign_in # FIXME: scope out this resource
+    #   resource :out, only: [ :show, :edit, :destroy ], controller: :sign_out
+    # end
+
   end
 
-  # You would confuse this naming, but it is natural to think about user's action.
-  resource :deactivate, except: :show
+
+  # For Staff
+  constraints ->(req) { req.host == ENV["RAILS_STAFF_URL"] } do
+    scope module: :staff, as: :staff do
+      # Health check
+      get "status" => "rails/health#show"
+      root to: "posts#index"
+      resource :deactivate, except: :show
+    end
+  end
 end
