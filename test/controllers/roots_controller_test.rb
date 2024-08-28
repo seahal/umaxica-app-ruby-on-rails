0dee8a56893d5d449f10c23a041186218b7036a5
SEAHAL <1926242+seahal@users.noreply.github.com>
2024-08-28 23:24:57 +0900

    added some tests for controllers.

--- test/controllers/roots_controller_test.rb
+++ test/controllers/roots_controller_test.rb
@@ -3,8 +3,16 @@
 require 'test_helper'
 
 class RootsControllerTest < ActionDispatch::IntegrationTest
+  def new_sign_1n_path
+    # code here
+  end
+
   test 'should get index' do
     get root_url
     assert_response :success
+
+    assert_select 'a[href=?]', root_path, count: 1
+    assert_select 'a[href=?]', new_sign_up_path, count: 1
+    assert_select 'a[href=?]', new_sign_in_path, count: 1
   end
 end
--- test/controllers/sign/in_controller_test.rb
+++ test/controllers/sign/in_controller_test.rb
@@ -4,8 +4,12 @@ require 'test_helper'
 
 module Sign
   class InControllerTest < ActionDispatch::IntegrationTest
-    # test "the truth" do
-    #   assert true
-    # end
+    test "new sign up page" do
+      get '/sign/up/new'
+      assert_response :success
+      assert_select "input[name=?]", 'user_email[email]', { count: 1 }
+      assert_select "input[type=submit]", { count: 1 }
+      assert_select 'a[href=?]', new_sign_in_path, count: 1
+    end
   end
 end
