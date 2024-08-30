ec158fb98e151f89b4132f31a69d714ed985733d
SEAHAL <1926242+seahal@users.noreply.github.com>
2024-08-30 09:49:07 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
-raise "[SAFEGUARD] db:seed is only use for dev env." unless Rails.env.development?
+raise "[SAFEGUARD] db:seed only use for dev env." unless Rails.env.development?
 
-StaffEmail.find_or_create_by(address: 'first.staff@example.com')
-UserEmail.find_or_create_by(address: 'first.user@example.com')
+staff = Staff.find_or_create_by!(id: '0191a0b6-1304-7c43-8248-0f13b4d29c57')
+user = User.find_or_create_by!(id: '0191a0b6-1304-7c43-8248-0f13b4d29c59')
+staff_email = StaffEmail.find_or_create_by!(address: 'first.staff@example.com')
+user_email = UserEmail.find_or_create_by!(address: 'first.user@example.com')
+staff_email.staff = staff
+user_email.user = user
+staff_email.save!
+user_email.save!
