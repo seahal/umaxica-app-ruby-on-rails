20dfcd1754ecef9936371a38c19075de33133095
SEAHAL <1926242+seahal@users.noreply.github.com>
2024-09-02 05:54:41 +0900

    i am creating Health Checker.

--- test/controllers/staff/healths_controller_test.rb
+++ test/controllers/staff/healths_controller_test.rb
@@ -4,6 +4,8 @@ class Staff::HealthsControllerTest < ActionDispatch::IntegrationTest
   test "should get show(json)" do
     get staff_healths_show_url, as: :json
     assert_response :success
+    json = JSON.parse(response.body)
+    assert_equal "OK", json['status']
   end
 
   test "should get show(html)" do
--- test/controllers/user/roots_controller_test.rb
+++ test/controllers/user/roots_controller_test.rb
@@ -4,5 +4,8 @@ class User::RootsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get user_root_url
     assert_response :success
+    assert_select "a[href=?]", user_root_path, count: 0
+    assert_select "a[href=?]", new_user_membership_path, count: 0
+    assert_select "a[href=?]", new_user_session_path, count: 0
   end
 end
