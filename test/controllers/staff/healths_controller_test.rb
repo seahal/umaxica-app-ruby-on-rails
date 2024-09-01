c1d804d5958d5f2c8675e90766a60f62a14ac007
SEAHAL <1926242+seahal@users.noreply.github.com>
2024-09-02 06:51:02 +0900

    [SYSTEM] You can run Rails on your env.

--- test/controllers/staff/healths_controller_test.rb
+++ test/controllers/staff/healths_controller_test.rb
@@ -1,15 +1,15 @@
 require "test_helper"
 
 class Staff::HealthsControllerTest < ActionDispatch::IntegrationTest
+  test "should get show(html)" do
+    get staff_healths_show_url
+    assert_response :success
+  end
+
   test "should get show(json)" do
     get staff_healths_show_url, as: :json
     assert_response :success
     json = JSON.parse(response.body)
-    assert_equal "OK", json['status']
-  end
-
-  test "should get show(html)" do
-    get staff_healths_show_url
-    assert_response :success
+    assert_equal "OK", json["status"]
   end
 end
