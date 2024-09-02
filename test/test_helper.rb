b48f95c7beeb895e0bdcafb734e161e9f9ac8a6c
SEAHAL <1926242+seahal@users.noreply.github.com>
2024-09-02 11:57:10 +0900

    began to use devcontainers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-# require 'simplecov'
-# SimpleCov.start 'rails' do
-#   coverage_dir './tmp/coverage'
-# end
+require 'simplecov'
+SimpleCov.start 'rails' do
+  coverage_dir './tmp/coverage'
+end
 
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
