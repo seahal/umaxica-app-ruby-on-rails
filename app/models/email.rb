f107c8d67bf281e7fe239d56854270d6db7102a2
SEAHAL <1926242+seahal@users.noreply.github.com>
2024-09-03 15:44:15 +0900

    I am creating sign up system

--- app/models/email.rb
+++ app/models/email.rb
@@ -4,8 +4,7 @@ class Email < ApplicationRecord
   has_one :user, foreign_key: "id"
   has_one :staff, foreign_key: "id"
 
-  validates :address, presence: true,
-                      length: { maximum: 255 },
+  validates :address, length: 3..255,
                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                       uniqueness: { case_sensitive: false }
 end
--- app/views/samples/_sample.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! sample, :id, :abc, :created_at, :updated_at
-json.url sample_url(sample, format: :json)
