8d303170e383ffc6e86e7a1701eba42588780adb
SEAHAL <1926242+seahal@users.noreply.github.com>
2024-09-03 14:22:48 +0900

    create new test.

--- test/controllers/user/membership_emails_controller_test.rb
+++ test/controllers/user/membership_emails_controller_test.rb
@@ -5,4 +5,22 @@ class User::MembershipEmailsControllerTest < ActionDispatch::IntegrationTest
     get new_user_registration_email_url
     assert_response :success
   end
+
+  test "valid signup information ?" do
+    get new_user_registration_email_path
+    assert_difference "UserEmail.count" do
+      post user_registration_emails_url, params: { user_email: {'address': "sample@example.com" }}
+    end
+    assert_redirected_to user_registration_email_path(UserEmail.last)
+    assert_response :redirect
+  end
+
+  test "same user_email is invalid, but no failure" do
+    UserEmail.create(address:'sample@example.com')
+    get new_user_registration_email_path
+    assert_no_difference "UserEmail.count" do
+      post user_registration_emails_url, params: { user_email: {'address': "sample@example.com" }}
+    end
+    assert_response :redirect
+  end
 end
