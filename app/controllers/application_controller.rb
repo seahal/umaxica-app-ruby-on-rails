b2f53d136c1e29cf94822a559f54b32f9e632a59
SEAHAL <1926242+seahal@users.noreply.github.com>
2024-09-07 13:42:33 +0900

    use different view layout from user/staff url.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # Change User's or Staff's Root Template.
   allow_browser versions: :modern
+
+  # Change User's or Staff's Root Template.
+  layout ->(controller) { request.host == ENV["RAILS_USER_URL"] ? "user/application" : "staff/application" }
 end
--- app/views/sign/up/show.json.jbuilder
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-json.partial! "samples/sample", user_email: @user_email
