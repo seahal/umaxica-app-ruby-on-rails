68575ac9b35664f3241574673ac7b5791b2d0adc
seahal <m.shiihara@gmail.com>
2025-02-02 03:15:42 +0900

    upgrade rails from 7.2 to 8.0

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,10 @@
-# frozen_string_literal: true
-
 require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed sign_in Gemfile, including any gems
+# Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
-if Rails.env.development? || Rails.env.test?
-  require "dotenv"
-
-  if File.exist?("/.dockerenv")
-    Dotenv.load ".env" # <= when you are sign_in docker
-  else
-    Dotenv.load ".env.local" # <= when you are sign_in mac, linux, and so on.
-  end
-end
 
 module Learn
   class Application < Rails::Application
@@ -30,14 +18,10 @@ module Learn
 
     # Configuration for the application, engines, and railties goes here.
     #
-    # These settings can be overridden sign_in specific environments using the files
-    # sign_in config/environments, which are processed later.
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.generators do |generate|
-      generate.orm :active_record, primary_key_type: :uuid
-    end
   end
 end
