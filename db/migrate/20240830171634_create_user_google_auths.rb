9cce0f861b521320552d44e6def4d8dfbb4a56c6
seahal <m.shiihara@gmail.com>
2025-02-03 05:18:05 +0900

    NOTE: my pc was hungup, and some data was deleted permanently.

--- db/migrate/20240830171634_create_user_google_auths.rb
+++ db/migrate/20240830171634_create_user_google_auths.rb
@@ -1,10 +1,10 @@
 class CreateUserGoogleAuths < ActiveRecord::Migration[7.2]
-#   def change
-#     create_table :user_google_auths, id: :uuid do |t|
-#       t.string :token
-#       t.references :user, null: false, foreign_key: true, type: :uuid
-#
-#       t.timestamps
-#     end
-#   end
- end
+  #   def change
+  #     create_table :user_google_auths, id: :uuid do |t|
+  #       t.string :token
+  #       t.references :user, null: false, foreign_key: true, type: :uuid
+  #
+  #       t.timestamps
+  #     end
+  #   end
+end
