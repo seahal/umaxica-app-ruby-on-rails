e902f4d1560fb76701384807ad7a4079269a5d05
seahal <seahal.com@gmail.com>
2025-02-11 12:52:18 +0900

    [SYSTEM] added hotwire.

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,2 +1,9 @@
-import "@hotwired/turbo-rails"
-import "controllers"
+import { Application } from "@hotwired/stimulus"
+
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+export { application }
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # FIXME: VERY POOR SECURITY
+    origins "*" # FIXME: VERY POOR SECURITY
 
     resource "*",
              headers: :any,
