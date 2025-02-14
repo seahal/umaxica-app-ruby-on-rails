fb6f0b8145100a5508585f3a0efd6f09d07eedbd
seahal <seahal.com@gmail.com>
2025-02-14 12:11:48 +0900

    [SYSTEM] I aded some files.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,9 +37,10 @@ Rails.application.configure do
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
 
-  # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3333 }
+  # Email Settings
+  ## Set localhost to be used by links generated in mailer templates.
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "localhost", port: 3333 }
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
@@ -51,6 +52,9 @@ Rails.application.configure do
     open_timeout:    5,
     read_timeout:    5
   }
+  ## Letter Opener => https://github.com/ryanb/letter_opener
+  #  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
