bd1990a15b49faa2a7be9c0e2b53527a11db7747
SEAHAL <1926242+seahal@users.noreply.github.com>
2025-02-27 05:49:43 +0900

    [UPDATE] fixed routing of homepages.

--- test/controllers/net/roots_controller_test.rb
+++ test/controllers/net/roots_controller_test.rb
@@ -6,8 +6,7 @@ module Net
   class RootsControllerTest < ActionDispatch::IntegrationTest
     test "should get index" do
       get net_root_url
-      assert_response :success
-      assert_select "a[href=?]", net_root_path, count: 2
+      assert_response :redirect
     end
   end
 end
