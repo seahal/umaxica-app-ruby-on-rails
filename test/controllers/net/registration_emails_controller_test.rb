4a924d713994d8f28b9517a9bce833b38ebcd3f9
SEAHAL <1926242+seahal@users.noreply.github.com>
2025-02-27 09:24:17 +0900

    [checkpoint] take a rest

--- test/controllers/net/registration_emails_controller_test.rb
+++ test/controllers/net/registration_emails_controller_test.rb
@@ -7,7 +7,6 @@ module Net
     test "should get new" do
       get new_net_registration_email_url
       assert_response :success
-
       assert_select "a[href=?]", net_root_path, count: 2
     end
 
@@ -20,6 +19,17 @@ module Net
       get new_net_registration_email_url
       assert_select "input#user_email_address"
       assert_select "input#user_email_confirm_policy"
+
+      assert_select "form[action=?][method=?]", net_registration_email_path, "post" do
+        # email入力フィールドの存在と属性チェック
+        assert_select "input[type=?][name=?]", "email", "user_email[address]"
+
+        # checkboxの存在と属性チェック
+        assert_select "input[type=?][name=?]", "checkbox", "user_email[confirm_policy]"
+
+        # submitボタンの存在
+        assert_select "input[type=?]", "submit"
+      end
     end
   end
 end
