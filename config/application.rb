3f828f251980699cd68f9a1cfad2537b90425808
SEAHAL <1926242+seahal@users.noreply.github.com>
2025-03-03 16:50:20 +0900

    [UPDATE] began to implement Rack Attack preferences

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module Learn
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Adde by user
+    config.middleware.use Rack::Attack
   end
 end
