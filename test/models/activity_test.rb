f0ca91d0e86ebc5c427719dab504c2408abd899c
SEAHAL <1926242+seahal@users.noreply.github.com>
2025-03-03 09:25:10 +0900

    [UPDATE] fixed github actions preference. (Re2)

--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -12,4 +12,9 @@ class ActivityTest < ActiveSupport::TestCase
     r = Redis.new(url: File.exist?("/.dockerenv") ? ENV["REDIS_SESSION_URL"] : "redis://localhost:6380/0")
     assert_equal r.ping, "PONG"
   end
+
+  test "checking PostgreSQL(AUTH, Pub) liveness" do
+    r = Redis.new(url: File.exist?("/.dockerenv") ? ENV["REDIS_CACHE_URL"] : "redis://localhost:6379/0")
+    assert_equal r.ping, "PONG"
+  end
 end
